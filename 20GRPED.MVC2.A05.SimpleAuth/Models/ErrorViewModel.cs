02c6d123cbbe8714cd68d5d09432ca144909c4ed
fdbva <villela.felipe@gmail.com>
2020-04-29 08:40:06 -0300

    Add project files.

---
+++ 20GRPED.MVC2.A05.SimpleAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _20GRPED.MVC2.A05.SimpleAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
